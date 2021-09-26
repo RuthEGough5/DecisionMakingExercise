3612f56b5e42f2297546453eabef347bc898c143
Ruth Gough <ruthegough5@gmail.com>
2021-09-25 22:25:23 -0500

    DecisionMakingExercise: pre-bonus

---
+++ script.js
@@ -0,0 +1,18 @@
+//1.
+let randomNum = Math.floor(Math.random() * 5 + 1);
+console.log(randomNum);
+//1b.IF Greater than or equal to 4
+if (6 + -1 >= 4) {console.log(true);}
+//ELSE IF Equal to 2 or 3
+else if (4 == 2 || 3) {console.log("Number is equal to 2 or 3");} 
+//ELSE Equal to 1
+else {console.log("Number equals 1");}
+//1c."Not equal to 3" if the value for randomNum is NOT equal to 3
+if (randomNum!=3)
+console.log("Not equal to 3");
+//1d.
+if (randomNum != 3 && 5)
+console.log("NOT equal to 3 AND not equal to 5");
+//1e.
+if (randomNum == 2||4)
+console.log("Equal to 2 OR equal to 4");
